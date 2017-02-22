078044faed5e4045e2945866c272ecfc51bef519
joellegg <joellegg@bellsouth.net>
2017-02-22 11:50:19 -0600

    able to add random events

--- www/js/calendar-ctrl.js
+++ www/js/calendar-ctrl.js
@@ -1,15 +1,14 @@
 controllerModule.controller('CalendarCtrl', function($scope, $location, MessageFactory, AuthFactory, $q) {
-  console.log('CalendarCtrl activated');
-
+  'use strict';
   $scope.calendar = {};
 
   $scope.changeMode = function(mode) {
     $scope.calendar.mode = mode;
-    console.log($scope.calendar)
   };
 
   $scope.loadEvents = function() {
     $scope.calendar.eventSource = createRandomEvents();
+    console.log('loadEvents')
   };
 
   $scope.onEventSelected = function(event) {
@@ -18,7 +17,6 @@ controllerModule.controller('CalendarCtrl', function($scope, $location, MessageF
 
   $scope.onViewTitleChanged = function(title) {
     $scope.viewTitle = title;
-    console.log($scope.viewTitle)
   };
 
   $scope.today = function() {
@@ -26,8 +24,8 @@ controllerModule.controller('CalendarCtrl', function($scope, $location, MessageF
   };
 
   $scope.isToday = function() {
-    var today = new Date();
-    var currentCalendarDate = new Date($scope.calendar.currentDate);
+    var today = new Date(),
+      currentCalendarDate = new Date($scope.calendar.currentDate);
 
     today.setHours(0, 0, 0, 0);
     currentCalendarDate.setHours(0, 0, 0, 0);
@@ -35,7 +33,44 @@ controllerModule.controller('CalendarCtrl', function($scope, $location, MessageF
   };
 
   $scope.onTimeSelected = function(selectedTime, events, disabled) {
-
+    console.log('Selected time: ' + selectedTime + ', hasEvents: ' + (events !== undefined && events.length !== 0) + ', disabled: ' + disabled);
   };
+
+  function createRandomEvents() {
+    var events = [];
+    for (var i = 0; i < 50; i += 1) {
+      var date = new Date();
+      var eventType = Math.floor(Math.random() * 2);
+      var startDay = Math.floor(Math.random() * 90) - 45;
+      var endDay = Math.floor(Math.random() * 2) + startDay;
+      var startTime;
+      var endTime;
+      if (eventType === 0) {
+        startTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + startDay));
+        if (endDay === startDay) {
+          endDay += 1;
+        }
+        endTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + endDay));
+        events.push({
+          title: 'All Day - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: true
+        });
+      } else {
+        var startMinute = Math.floor(Math.random() * 24 * 60);
+        var endMinute = Math.floor(Math.random() * 180) + startMinute;
+        startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + startDay, 0, date.getMinutes() + startMinute);
+        endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + endDay, 0, date.getMinutes() + endMinute);
+        events.push({
+          title: 'Event - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: false
+        });
+      }
+    }
+    return events;
+  }
 
 })
